1f54e83234819650330fa5821ab57a9724c079e0
karpik122 <karpik1771@gmail.com>
2026-07-20 12:47:17 +0200

    main

---
+++ src/js/dane.js
@@ -0,0 +1,23 @@
+const opisyProjektow = {
+  "images/spyxfamily1.png": {
+    opis: "Cyfrowy rysunek Anyi na czarnym tle."
+  },
+  "images/spyxfamily2.png": {
+    opis: "Wersja rysunku Anyi z jaskrawym, kolorowym tłem."
+  },
+  "images/anime.jpg": {
+    opis: "Rysunek dziewczyny w stylu anime z serduszkami."
+  },
+  "images/heart.jpg": {
+    opis: "Szkic mrocznego pluszaka z czerwonym sercem."
+  },
+  "images/Horimiya.jpg": {
+    opis: "Ilustracja przedstawiająca przytulającą się parę z anime."
+  },
+  "images/mcDuck.jpg": {
+    opis: "Ołówkowy szkic Sknerusa McKwacza w cylindrze."
+  },
+  "images/szkic.jpg": {
+    opis: "Prosty szkic postaci na kartce w kratkę."
+  }
+};
